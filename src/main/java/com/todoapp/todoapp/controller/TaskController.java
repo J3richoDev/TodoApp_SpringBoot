69a0d1e80911b3daca42f3d771cb969ebf998479
J3richoDev <nomat.dev@gmail.com>
2025-01-15 16:39:20 +0800

    done

--- src/main/java/com/todoapp/todoapp/controller/TaskController.java
+++ src/main/java/com/todoapp/todoapp/controller/TaskController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.todoapp.todoapp.models.Task;
 import com.todoapp.todoapp.services.TaskServices;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class TaskController {
@@ -23,4 +27,23 @@ public class TaskController {
         model.addAttribute("tasks", tasks);
         return "Task_List";
     }
+
+    @PostMapping
+    public String createTask(@RequestParam String title) {
+        taskServices.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskServices.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/complete")
+    public String completeTask(@PathVariable Long id){
+        taskServices.completeTask(id);
+        return "redirect:/";
+    }
+    
 }
--- src/main/java/com/todoapp/todoapp/services/TaskServices.java
+++ src/main/java/com/todoapp/todoapp/services/TaskServices.java
@@ -20,4 +20,22 @@ public class TaskServices {
         return taskRepository.findAll();
     }
 
+    public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void completeTask(Long id) {
+        Task task = taskRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid task id"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+    }
+
 }
